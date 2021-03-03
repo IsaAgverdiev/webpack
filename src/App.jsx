5fa086dfedef7c11f85f3a29577afdf401d13648
Isa <74831265+IsaAgverdiev@users.noreply.github.com>
2021-03-03 11:45:30 +0300

    ilitial webpack config

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import "./App.scss";
+import Aside from "./components/Aside";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      {/* <Header />
+      <Aside />
+      <Profile /> */}
+    </div>
+  );
+}
+
+export default App;
